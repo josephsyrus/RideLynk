2ade53897d675da4d890136a75a7d11c6a090b99
sivasangar2006 <sivasangar2786@gmail.com>
2025-03-23 23:09:53 +0530

    sign-in part UI over

---
+++ script.js
@@ -0,0 +1,14 @@
+const signinbtn = document.getElementById("show-signin-btn");
+const signupbtn = document.getElementById("show-signup-btn");
+
+const signin = document.getElementById("sign-in-div");
+const signup = document.getElementById("sign-up-div");
+
+signinbtn.addEventListener("click",function(){
+    signin.style.display = "block";
+    signup.style.display = "none";
+})
+signupbtn.addEventListener("click",function(){
+    signup.style.display = "block";
+    signin.style.display = "none";
+})
